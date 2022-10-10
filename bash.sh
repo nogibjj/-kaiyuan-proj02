b0ee2692f6a700f1b1a8082526359d4ebbf7b2f2
Kaiyuan Li <35396906+Apple-Pie-smile@users.noreply.github.com>
2022-10-10 02:14:29 +0000

    modify bash.sh

--- bash.sh
+++ bash.sh
@@ -4,10 +4,33 @@ echo "enter your csv filename"
 read filename
 if [ -e "$filename" ]
 then
+    #truncating data
+    echo "truncating data"
+    head -n 3 $filename > truncatingResults.txt
+    #cut -d',' -f2 $filename > truncatingResults.txt
+    echo ""
+    
+    # sort by years
+    echo "sort by year"
+    sort -nr -t ',' -k4 $filename > sortResults1.csv
+    echo ""
+    
+    # delete rows that contains ""
+    echo "clean data"
+    sed '/"/d' $filename > cleanResults.csv
+    #sed -i -e '/""/d' $filename
+    echo "" 
+
+    # sort by years
+    echo "sort by year again"
+    sort -nr -t ',' -k4 cleanResults.csv > sortResults2.csv
+    echo ""
+
     # filter and save as csv file
+    echo "ETL"
 	echo "enter your key word for filtering"
 	read keyword
-	grep -i "$keyword" "$filename" > filterResults.csv
+	grep -i $keyword $filename > filterResults.csv
     
     # print filter results
     while IFS="," read -r rank name platform year remaining
@@ -19,7 +42,7 @@ then
     done < <(tail -n +2 filterResults.csv)
     
     # count how many results in total
-    echo $(wc -l < filterResults.csv) "results in total"
+    echo $(wc -l < filterResults.csv) "filter results in total"
 else
 	echo "the filename you entered does not exist"
 fi
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pandas
+import typer
+
+app = type.Typer()
+
+@app.command()
+def showAll():
+    df = pf.read()
